4b9081748eb8132b14326d1dc38087bafc41fbea
HaoliangZhou <89777862+HaoliangZhou@users.noreply.github.com>
2023-05-09 17:15:44 +0800

    Update DualATTNet.py

--- model/DualATTNet.py
+++ model/DualATTNet.py
@@ -16,9 +16,9 @@ class DualATTNet(nn.Module):
             Inception(in_channels=3, out_channels=6),
             Inception(in_channels=24, out_channels=12),
             nn.MaxPool2d(kernel_size=2),
-            # nn.Conv2d(in_channels=48, out_channels=96, kernel_size=1),  # 1*1+BN+ReLU=0.7127,0.6806,0.6814
-            # nn.BatchNorm2d(96),  # 1*1+BN=0.7466,0.7190,0.7186
-            # nn.ReLU(inplace=True),  # 1*1+ReLU=0.7330,0.6917,0.6926
+            # nn.Conv2d(in_channels=48, out_channels=96, kernel_size=1), 
+            # nn.BatchNorm2d(96), 
+            # nn.ReLU(inplace=True), 
             CBAM(48),
         )
 
@@ -35,9 +35,9 @@ class DualATTNet(nn.Module):
         # fc
         self.fc = torch.nn.Sequential(
             torch.nn.Linear(in_features=14 * 14 * (256 + 256), out_features=1024),
-            # torch.nn.BatchNorm1d(1024),  # 仅BN 70不到
+            # torch.nn.BatchNorm1d(1024), 
             torch.nn.ReLU(inplace=True),
-            torch.nn.Dropout(0.5),  # 仅Drop0.7370
+            torch.nn.Dropout(0.5), 
             torch.nn.Linear(in_features=1024, out_features=num_classes),
         )
 
@@ -67,9 +67,9 @@ class DualATTNet_IncepCBAM(nn.Module):
             Inception(in_channels=3, out_channels=6),
             Inception(in_channels=24, out_channels=12),
             nn.MaxPool2d(kernel_size=2),
-            # nn.Conv2d(in_channels=48, out_channels=96, kernel_size=1),  # 1*1+BN+ReLU=0.7127,0.6806,0.6814
-            # nn.BatchNorm2d(96),  # 1*1+BN=0.7466,0.7190,0.7186
-            # nn.ReLU(inplace=True),  # 1*1+ReLU=0.7330,0.6917,0.6926
+            # nn.Conv2d(in_channels=48, out_channels=96, kernel_size=1),
+            # nn.BatchNorm2d(96), 
+            # nn.ReLU(inplace=True),
             CBAM(48),
         )
 
@@ -95,49 +95,7 @@ class DualATTNet_IncepCBAM(nn.Module):
 class DualATTNet_ViT(nn.Module):
     def __init__(self, in_channels=3, num_classes=3):
         super(DualATTNet_ViT, self).__init__()
-
-        # vit_pc, 原来无msad的vit
-        # self.vit_pos = VisionTransformer_POS(img_size=14,
-        #                                      patch_size=1, in_chans=3, embed_dim=512, depth=3, num_heads=4, mlp_ratio=2,
-        #                                      qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6),
-        #                                      drop_path_rate=0.3)
-        self.vit_pos = VisionTransformer_POS(img_size=14, patch_size=1, in_chans=3, embed_dim=512, depth=3,
-                                             num_heads=4, mlp_ratio=2, qkv_bias=True,
-                                             norm_layer=partial(nn.LayerNorm, eps=1e-6),
-                                             drop_path_rate=0.3, head_drop_type='HeadDropOut', head_drop_rate=0.3,
-                                             head_drop_upper=1)
-
-        self.resize = Resize([14, 14])
-
-        self.conv1x1_pc = nn.Conv2d(in_channels=512, out_channels=256, kernel_size=1)
-
-        # fc
-        self.fc = torch.nn.Sequential(
-            torch.nn.Linear(in_features=14 * 14 * (256), out_features=1024),
-            torch.nn.BatchNorm1d(1024),
-            torch.nn.ReLU(inplace=True),
-            torch.nn.Dropout(0.5),
-            torch.nn.Linear(in_features=1024, out_features=num_classes),
-        )
-
-    def forward(self, x):
-        B = x.shape[0]
-        x = self.vit_pos(self.resize(x)).transpose(1, 2).view(B, 512, 14, 14)
-        x = self.conv1x1_pc(x)
-        x = x.reshape(x.shape[0], -1)  # flatten 变成全连接层的输入
-        x = self.fc(x)
-
-        return x
-
-class DualATTNet_ViT1(nn.Module):
-    def __init__(self, in_channels=3, num_classes=3):
-        super(DualATTNet_ViT1, self).__init__()
-
-        # vit_pc, 原来无msad的vit
-        # self.vit_pos = VisionTransformer_POS(img_size=14,
-        #                                      patch_size=1, in_chans=3, embed_dim=512, depth=3, num_heads=4, mlp_ratio=2,
-        #                                      qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6),
-        #                                      drop_path_rate=0.3)
+        
         self.vit_pos = VisionTransformer_POS(img_size=14, patch_size=1, in_chans=3, embed_dim=512, depth=3,
                                              num_heads=4, mlp_ratio=2, qkv_bias=True,
                                              norm_layer=partial(nn.LayerNorm, eps=1e-6),
